b77b6784d0ddd408ebb051b69da719707f90e0e1
lijinmin <lijinmin3903@126.com>
2025-08-01 16:39:37 +0800

    m

--- utils/data_loading.go
+++ utils/data_loading.go
@@ -1,1 +1,26 @@
 package utils
+
+type BasicDataset struct {
+	IDs        []string
+	imagesDir  string
+	maskDir    string
+	Scale      float64
+	maskSuffix string
+}
+
+func (data *BasicDataset) NewDataset() *BasicDataset {
+	return &BasicDataset{}
+}
+
+func (data *BasicDataset) Init() {
+
+}
+func (data *BasicDataset) loadImage() {
+
+}
+func (data *BasicDataset) preProcess() {
+
+}
+func (data *BasicDataset) getitem(id string) {
+
+}
